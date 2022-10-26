1a1651eb7defd37121e4b7318ea453d5ce5ddaa3
vva07 <vva0779@yandex.ru>
2022-10-26 16:52:35 +0300

    testing plots

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ R=15 Ом; L=0.0015 мГн;
 U=100 В;
 i[L](t) - ?
 """
-
+import task1
 
 if __name__ == "__main__":
-    pass
+    task1.basic_plot()
---
+++ task1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plotter
+
+
+def basic_plot():
+    plotter.plot([1, 2, 3, 4], [1.5, 1.9, 3.6, 4.9])
+    plotter.show()
+
