07775017e1c21d2318e23b465d7a09a5b77a17aa
vva07 <vva0779@yandex.ru>
2022-11-24 19:34:58 +0300

    Lab2 blueprint

---
+++ Lab2/main.py
@@ -0,0 +1,13 @@
+"""
+R=15 Ом; L=0.0015 мГн;
+U=100 В;
+i[L](t) - ?
+"""
+
+
+if __name__ == "__main__":
+    """print("Task 1 started... \n")
+    task1.run_all()
+    print("\n Task 2 started... \n")
+    task2.run_all()"""
+
