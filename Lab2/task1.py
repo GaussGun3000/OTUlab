f72fc047bd307380f81e161847de34fc1b0c213e
vva07 <vva0779@yandex.ru>
2022-11-29 18:08:29 +0300

    WaterTank class defined

--- Lab2/task1.py
+++ Lab2/task1.py
@@ -11,5 +11,28 @@ h = 9;     q = 4;
 3. Определить параметры ПИД-регулятора при наименьшем времени регулирования в системе. При полученных
  параметрах определить запасы устойчивости по амплитуде и фазе, степень устойчивости и колебательности.
  Сделать выводы.
-
 """
+
+WATER_TARGET_LEVEL = 9
+
+
+class WaterTank:
+    def __init__(self):
+        self.water_level = WATER_TARGET_LEVEL
+
+    def update(self, inlet: int, dt: float):
+        """
+        Update state of the object based on water inlet amount as control input
+
+        :param inlet: water inlet amount (m^3/s)
+        :param dt: time span in seconds
+        :return:
+        """
+        if inlet >= 0:
+            pass
+        else:
+            raise ValueError(f"WaterTank.update(): Value of inlet param can not be negative (got {inlet})")
+
+
+def run():
+    pass
